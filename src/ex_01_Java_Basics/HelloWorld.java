0e8374bf85462e95937fe8fb4e2c9f569c9a66bf
sk11verma <sk11verma@gmail.com>
2025-06-21 12:20:29 +0530

    Initial commit

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -5,6 +5,8 @@ import com.sun.tools.javac.Main;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello. Welcome to Java World");
+        int a=5;
+        System.out.println(a);
     }
 
 }
