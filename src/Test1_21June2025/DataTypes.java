e4ededcb26988960fc6b43cc49576d8bbf6c2579
sk11verma <sk11verma@gmail.com>
2025-06-21 12:20:07 +0530

    Initial commit

---
+++ src/Test1_21June2025/DataTypes.java
@@ -0,0 +1,17 @@
+package Test1_21June2025;
+
+public class DataTypes {
+    public static void main(String[] args){
+
+    int a=10;
+    char b='b';
+    Boolean c=true;
+    double d=3.14;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+
+}
+
+}
