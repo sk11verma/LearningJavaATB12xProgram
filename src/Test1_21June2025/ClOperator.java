1669a47988f905653b829e69ca4f6489e59ec3f7
sk11verma <sk11verma@gmail.com>
2025-06-21 12:37:59 +0530

    Initial commit

---
+++ src/Test1_21June2025/ClOperator.java
@@ -0,0 +1,16 @@
+package Test1_21June2025;
+
+public class ClOperator {
+    public static void main(String[] args) {
+        int a=2;
+        int b=2;
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+
+
+    }
+}
