7e957dea6abb0b63983f464a7771aadd0f069cd9
sk11verma <sk11verma@gmail.com>
2025-06-21 17:40:12 +0530

    Initial commit

--- src/Test1_21June2025/PrimeNumbers.java
+++ src/Test1_21June2025/PrimeNumbers.java
@@ -8,7 +8,7 @@ public class PrimeNumbers {
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
         int count=0;
-        for(int i=2;i<=num/2;i++)
+        for(int i=2;i<=num;i++)
         {
             if (num%i==0)
             count++;
@@ -16,5 +16,9 @@ public class PrimeNumbers {
     if (count>1)
         System.out.println("Number is not prime");
     else System.out.println("Number is prime");
+
+
+        }
     }
-}
+
+
