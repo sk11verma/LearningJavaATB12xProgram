2f57c692d52bbc12705ee41430221e36ee164972
sk11verma <sk11verma@gmail.com>
2025-06-21 12:20:29 +0530

    Initial commit

---
+++ src/Test1_21June2025/ArithmeticTest.java
@@ -0,0 +1,19 @@
+package Test1_21June2025;
+
+public class ArithmeticTest {
+    public static void main(String[] args) {
+        int a=10;
+        int b=4;
+        System.out.println(a/b);
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a%b);
+        System.out.println(a*b);
+        System.out.println(a+=b);
+        System.out.println(a-=b);
+        System.out.println(a*=b);
+        System.out.println(a/=b);
+
+
+    }
+}
