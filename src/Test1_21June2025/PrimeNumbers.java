7e1a57116022c0461dff0270fa8931b02bfd4440
sk11verma <sk11verma@gmail.com>
2025-06-21 17:14:18 +0530

    Initial commit

---
+++ src/Test1_21June2025/PrimeNumbers.java
@@ -0,0 +1,20 @@
+package Test1_21June2025;
+
+import java.util.Scanner;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number");
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int count=0;
+        for(int i=2;i<=num/2;i++)
+        {
+            if (num%i==0)
+            count++;
+    }
+    if (count>1)
+        System.out.println("Number is not prime");
+    else System.out.println("Number is prime");
+    }
+}
