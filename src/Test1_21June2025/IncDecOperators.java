ef41de0b009ff41b587fd6437482353d3e680849
sk11verma <sk11verma@gmail.com>
2025-06-21 12:46:14 +0530

    Initial commit

---
+++ src/Test1_21June2025/IncDecOperators.java
@@ -0,0 +1,12 @@
+package Test1_21June2025;
+
+public class IncDecOperators {
+    public static void main(String[] args) {
+        int i=1;
+       int a=i++;//assign then increment 1
+       int b=++i;//increment then assign ++2=3
+       int c=i--;//Assign then decrement 3
+       int d=--i;// decrement then assign 2--=1
+        System.out.println(a+","+b+","+c+","+d);
+    }
+}
