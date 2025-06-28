75e196ee3ef357d9ac9bc884c5d573bfa0676d3c
sk11verma <sk11verma@gmail.com>
2025-06-28 11:34:55 +0530

    Initial commit

---
+++ src/Test3_28June2025/stringLearning.java
@@ -0,0 +1,11 @@
+package Test3_28June2025;
+
+public class stringLearning {
+    public static void main(String[] args) {
+        String s1=new String("Suresh");
+        System.out.println(s1.concat("Verma"));
+        System.out.println(s1.length());
+        System.out.println(s1.charAt(3));
+        System.out.println(s1.contains("res"));
+    }
+}
