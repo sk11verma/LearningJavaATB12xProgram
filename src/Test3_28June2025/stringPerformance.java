6ed498fb158b0493e4f3592f816fb3a0ad326bd8
sk11verma <sk11verma@gmail.com>
2025-06-28 12:02:04 +0530

    Initial commit

--- src/Test3_28June2025/stringPerformance.java
+++ src/Test3_28June2025/stringPerformance.java
@@ -2,10 +2,26 @@ package Test3_28June2025;
 
 public class stringPerformance {
     public static void main(String[] args) {
+        int N = 1_000_000;
+        long start, elapsed;
         String s1=new String("Suresh");
-        s1+="Verma";
-        System.out.println(s1);
-        System.out.println(s1.concat("Verma"));
-        System.out.println(s1);
+        start = System.nanoTime();
+        for (int i = 0; i < N; i++) s1.concat("x");
+        elapsed = System.nanoTime() - start;
+        System.out.printf("String: %.2f ms%n", elapsed / 1e6);
+        StringBuilder sb = new StringBuilder("Hello");
+        start = System.nanoTime();
+        for (int i = 0; i < N; i++) sb.append("x");
+        elapsed = System.nanoTime() - start;
+        System.out.printf("StringBuilder: %.2f ms%n", elapsed / 1e6);
+
+
+        // Using StringBuffer (thread-safe, but slower)
+        StringBuffer sbf = new StringBuffer("Hello");
+        start = System.nanoTime();
+        for (int i = 0; i < N; i++) sbf.append("x");
+        elapsed = System.nanoTime() - start;
+        System.out.printf("StringBuffer: %.2f ms%n", elapsed / 1e6);
+
     }
 }
